e2bb1a254200d39f7333ccc67913813abe725833
vairodp <vairo.dp@gmail.com>
2023-01-18 13:26:13 +0100

    Updated solver

--- CP/src/solver.py
+++ CP/src/solver.py
@@ -3,14 +3,18 @@ import os
 # 1 minute
 from utils import TIME_LIMIT
 
-def solver(file_path, output, rotation, model_name, symmetry_breaking_constraints):
-    print("Solving " + file_path + " with model + " + model_name)
-    cmd = "minizinc " + model_name + ' ' + file_path + " --solver Chuffed --solver-time-limit " + str(TIME_LIMIT)
+# TODO implement different solvers
+def solver(file_path, output_path, rotation, model_name, symmetry_breaking_constraints):
+    
+    print("---> Solving " + file_path)
 
-    stream = os.popen(cmd)
-    output = stream.read()
-    print(output)
+    cmd = "minizinc " + model_name + ' ' + file_path + " --solver Gecode -f --output-time --solver-time-limit " + str(TIME_LIMIT)
+    stream = os.popen(cmd)  
 
-    print("Done solving " + file_path)
+    output_file = os.path.join(output_path, os.path.basename(file_path).replace(".dzn", ".txt"))
 
-    return
+    with open(output_file, 'w') as f:
+        # Write the output to the file
+        f.write(stream.read())
+
+    return output_file
