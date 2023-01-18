31209d9ba74aacbb44c563c6d0076a52b4b2adcf
vairodp <vairo.dp@gmail.com>
2023-01-18 13:30:03 +0100

    Added proto SAT and proto MIP

---
+++ MIP/src/MIPsolver.py
@@ -0,0 +1,5 @@
+import os
+from utils import TIME_LIMIT
+
+def MIPsolver(data, output, rotation, model_name, symmetry_breaking_constraints):
+    return
---
+++ SAT/src/SATsolver.py
@@ -0,0 +1,4 @@
+import os
+from utils import TIME_LIMIT
+def SATsolver(file):
+    return
