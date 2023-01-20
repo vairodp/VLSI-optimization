f3a81df0fba7a0f95decbe0692a3cfcac7e6de2a
vairodp <vairo.dp@gmail.com>
2023-01-20 18:14:12 +0100

    Added working SAT solver utility

--- SAT/src/SATsolver.py
+++ SAT/src/SATsolver.py
@@ -1,4 +1,40 @@
 import os
-from utils import TIME_LIMIT
-def SATsolver(file):
+import time
+import json
+#from utils import TIME_LIMIT
+
+def read_variables(file_path):
+    variables = {}
+    try:
+        with open(file_path, 'r') as f:
+            for line in f:
+                # Split the line by '=' and remove the ';' from the end
+                var, value = line.strip().split('=')[0],line.strip().split('=')[1].strip(';')
+                # check if value is a list 
+                if '[' in value:
+                    # split the values by ',' and convert them to int
+                    value = list(map(int, value[1:-1].split(',')))
+                elif value.isdigit():
+                    value = int(value)
+                # Add the variable and value to the dictionary
+                variables[var] = value
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except:
+        print("An error occurred.")
+    return variables
+
+def SATsolver(file_path, output_path, rotation, model_path, symmetry_breaking_constraints):
+    
+    circuits_variables = read_variables(file_path)
+
+    cmd = "python " + model_path + " " + file_path
+    stream = os.popen(cmd)  
+
+    output_file = os.path.join(output_path, os.path.basename(file_path).replace(".dzn", ".txt"))
+
+    with open(output_file, 'w') as f:
+        # Write the output to the file
+        f.write(stream.read())
+    
     return
